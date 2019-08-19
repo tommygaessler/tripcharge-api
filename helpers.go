fb73c3c89d9e3543c5467b2f29a88c2d0fa4a96e
Tommy Gaessler <tommy.gaessler@gmail.com>
2019-08-18 18:27:00 -0600

    updated name

---
+++ helpers.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func StationToJson(stations []Station) string {
+	byte, err := json.Marshal(stations)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return string(byte)
+}
+
+func StationToJsonWStartEnd(stations []Station, startLat, startLng, endLat, endLng float64) string {
+	byte, err := json.Marshal(stations)
+	if err != nil {
+		fmt.Println(err)
+	}
+	stringWO := string(byte)[0 : len(string(byte))-1]
+	stringW := stringWO + fmt.Sprintf(`,{"startingEndCords": {"startLat": %s, "startLng": %s, "endLat": %s, "endLng": %s}}`, toString(startLat), toString(startLng), toString(endLat), toString(endLng)) + "]"
+	return stringW
+}
+
+func urlGetter(url string) []Station {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println("Bad")
+		panic(err)
+	}
+
+	jsonDataFromHttp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Bad")
+		panic(err)
+	}
+	var jsonData []Station
+	err = json.Unmarshal([]byte(jsonDataFromHttp), &jsonData)
+	if err != nil {
+		fmt.Println("Bad")
+		panic(err)
+	}
+	return jsonData
+}
+
+func latLngGetter(url string) (lat float64, lng float64) {
+	resp, err := http.Get(url)
+	defer resp.Body.Close()
+	if err != nil {
+		fmt.Println("Bad")
+		panic(err)
+	}
+	jsonDataFromHttp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Bad")
+		panic(err)
+	}
+	if string(jsonDataFromHttp[:][46:48]) == "[]" {
+		return 0, 0
+	}
+	var jsonData addressToLatLng
+	err = json.Unmarshal([]byte(jsonDataFromHttp), &jsonData)
+	if err != nil {
+		fmt.Println("Bad")
+		panic(err)
+	}
+	return jsonData.Results[0].Geometry.Location.Lat, jsonData.Results[0].Geometry.Location.Lng
+}
+
+func keepLines(s string, n int) string {
+	result := strings.Join(strings.Split(s, "\n")[:n], "\n")
+	return strings.Replace(result, "\r", "", -1)
+}
+
+// add data to here
+type Station struct {
+	UsageCost string `json:"UsageCost"`
+	GeneralComments string `json:"GeneralComments"`
+	AddressInfo struct {
+		AddressLine1    string  `json:"AddressLine1"`
+		Latitude        float64 `json:"Latitude"`
+		Longitude       float64 `json:"Longitude"`
+		Postcode        string  `json:"Postcode"`
+		StateOrProvince string  `json:"StateOrProvince"`
+		Town            string  `json:"Town"`
+		AccessComments            string  `json:"AccessComments"`
+		// Connector       string  `json:"Connections[0].ConnectionType.Title"`
+		// Level           string  `json:"Connections[0].Level.Title"`
+		// Quantity     string  `json:"Connections[0].Quantity"`
+	} `json:"AddressInfo"`
+	OperatorInfo struct {
+		Title    string  `json:"Title"`
+	} `json:"OperatorInfo"`
+	UsageType struct {
+		Title    string  `json:"Title"`
+	} `json:"UsageType"`
+	Connections []Connection
+}
+
+type Connection struct {
+	ConnectionType struct {
+		Title string `json:"Title"`
+	} `json:"ConnectionType"`
+	Level struct {
+		Title string `json:"Title"`
+	} `json:"Level"`
+	Quantity float64 `json:"Quantity"`
+}
+
+type addressToLatLng struct {
+	Results []struct {
+		Geometry struct {
+			Location struct {
+				Lat float64 `json:"lat"`
+				Lng float64 `json:"lng"`
+			} `json:"location"`
+		} `json:"geometry"`
+	} `json:"results"`
+}
+
+func getDisanceBetween(lat1 float64, long1 float64, lat2 float64, long2 float64) float64 {
+	var dLat float64 = deg2rad(lat2 - lat1)
+	var dLon float64 = deg2rad(long2 - long1)
+	var a float64 = math.Sin(dLat/2)*math.Sin(dLat/2) +
+		math.Cos(deg2rad(lat1))*math.Cos(deg2rad(lat2))*
+			math.Sin(dLon/2)*math.Sin(dLon/2)
+	var c = 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+	var d = 3961 * c
+	return d
+}
+
+func deg2rad(deg float64) float64 {
+	return deg * (math.Pi / 180)
+}
+
+func getStationsBetween(lat1 float64, long1 float64, lat2 float64, long2 float64, stations []Station, endDistance float64) []Station {
+	var output []Station
+
+	num := len(stations)
+	for i := 0; i < num; i++ {
+		stationDist := getDisanceBetween(lat2, long2, stations[i].AddressInfo.Latitude, stations[i].AddressInfo.Longitude)
+		startToStation := getDisanceBetween(lat1, long1, stations[i].AddressInfo.Latitude, stations[i].AddressInfo.Longitude)
+		if endDistance > stationDist && (stationDist+startToStation) < endDistance*1.07 {
+			output = append(output, stations[i])
+		}
+	}
+	return output
+}
+
+func getMaxStations(num float64) string {
+	return "9999"
+}
+
+func toString(num float64) string {
+	return strconv.FormatFloat(num, 'f', -1, 64)
+}
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/lat/{startLat}/long/{startLong}", getLatLong).Methods("GET")
+	router.HandleFunc("/start/lat/{startLat}/long/{startLong}/end/lat/{endLat}/long/{endLong}", getBetween).Methods("GET")
+	router.HandleFunc("/start/address/{address1}/end/address/{address2}", getAddresses).Methods("GET")
+	router.HandleFunc("/start/lat/{startLat}/long/{startLong}/end/address/{address}", curLocationStart).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+}
+
+func getLatLong(res http.ResponseWriter, req *http.Request) {
+	startLat := mux.Vars(req)["startLat"]
+	startLong := mux.Vars(req)["startLong"]
+
+	url := fmt.Sprintf("http://api.openchargemap.io/v2/poi/?output=json&distance=100&maxresults=50&latitude=%s&longitude=%s", startLat, startLong)
+	datas := urlGetter(url)
+	output := StationToJson(datas)
+	res.Header().Set("Content-Type", "application/json")
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintln(res, output)
+}
+
+func getBetween(res http.ResponseWriter, req *http.Request) {
+	startLat, _ := strconv.ParseFloat(mux.Vars(req)["startLat"], 64)
+	startLong, _ := strconv.ParseFloat(mux.Vars(req)["startLong"], 64)
+	endLat, _ := strconv.ParseFloat(mux.Vars(req)["endLat"], 64)
+	endLong, _ := strconv.ParseFloat(mux.Vars(req)["endLong"], 64)
+
+	num := getDisanceBetween(startLat, startLong, endLat, endLong)
+	maxStations := getMaxStations(num)
+	fmt.Println(num)
+	url := fmt.Sprintf("http://api.openchargemap.io/v2/poi/?output=json&latitude=%s&longitude=%s&distance=%s&&maxresults=%s", toString(startLat), toString(startLong), toString(num), maxStations)
+	datas := urlGetter(url)
+	allBetween := getStationsBetween(startLat, startLong, endLat, endLong, datas, num)
+	output := StationToJsonWStartEnd(allBetween, startLat, startLong, endLat, endLong)
+
+	res.Header().Set("Content-Type", "application/json")
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprint(res, output)
+}
+
+func getAddresses(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+	address1, _ := mux.Vars(req)["address1"]
+	address1 = strings.Replace(address1, " ", "+", -1)
+	address1 = fmt.Sprintf("https://maps.googleapis.com/maps/api/place/textsearch/json?query=%s&key=%s", address1, os.Getenv("googleMapsApiKey"))
+
+	address1Lat, address1Lng := latLngGetter(address1)
+
+	address2, _ := mux.Vars(req)["address2"]
+	address2 = strings.Replace(address2, " ", "+", -1)
+	address2 = fmt.Sprintf("https://maps.googleapis.com/maps/api/place/textsearch/json?query=%s&key=%s", address2, os.Getenv("googleMapsApiKey"))
+	address2Lat, address2Lng := latLngGetter(address2)
+	if address1Lat == 0 || address1Lng == 0 || address2Lat == 0 || address2Lng == 0 {
+		fmt.Fprint(res, "[]")
+	} else {
+		num := getDisanceBetween(address1Lat, address1Lng, address2Lat, address2Lng)
+		maxStations := getMaxStations(num)
+
+		url := fmt.Sprintf("http://api.openchargemap.io/v2/poi/?output=json&latitude=%s&longitude=%s&distance=%s&&maxresults=%s", toString(address1Lat), toString(address1Lng), toString(num), maxStations)
+		datas := urlGetter(url)
+		allBetween := getStationsBetween(address1Lat, address1Lng, address2Lat, address2Lng, datas, num)
+		output := StationToJsonWStartEnd(allBetween, address1Lat, address1Lng, address2Lat, address2Lng)
+
+		fmt.Fprint(res, output)
+	}
+}
+func curLocationStart(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	res.Header().Set("Access-Control-Allow-Origin", "*")
+	startLat, _ := strconv.ParseFloat(mux.Vars(req)["startLat"], 64)
+	startLong, _ := strconv.ParseFloat(mux.Vars(req)["startLong"], 64)
+
+	address, _ := mux.Vars(req)["address"]
+	address = strings.Replace(address, " ", "+", -1)
+	address = fmt.Sprintf("https://maps.googleapis.com/maps/api/place/textsearch/json?query=%s&key=%s", address, os.Getenv("googleMapsApiKey"))
+	addressLat, addressLng := latLngGetter(address)
+	if startLat == 0 || startLong == 0 || addressLat == 0 || addressLng == 0 {
+		fmt.Fprint(res, "[]")
+	} else {
+		num := getDisanceBetween(startLat, startLong, addressLat, addressLng)
+		maxStations := getMaxStations(num)
+
+		url := fmt.Sprintf("http://api.openchargemap.io/v2/poi/?output=json&latitude=%s&longitude=%s&distance=%s&&maxresults=%s", toString(startLat), toString(startLong), toString(num), maxStations)
+		datas := urlGetter(url)
+		allBetween := getStationsBetween(startLat, startLong, addressLat, addressLng, datas, num)
+		output := StationToJsonWStartEnd(allBetween, startLat, startLong, addressLat, addressLng)
+
+		fmt.Fprint(res, output)
+	}
+}
